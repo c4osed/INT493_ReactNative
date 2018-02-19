e0897b783c8b9913e2844d0a8419b34d2e465e40
Zatic Prasertarcha <coach.psac@gmail.com>
2018-02-19 11:38:06 +0700

    add background image

---
+++ WeatherProject.js
@@ -0,0 +1,80 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ImageBackground
+} from 'react-native';
+
+type Props = {};
+export default class WeatherProject extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { zip: "" };
+    console.log("zip "+this.state.zip) ;
+  }
+
+  _handleTextChange = event => {
+    this.setState({ zip: event.nativeEvent.text });
+    console.log("onSubmit") ;
+  };
+
+  render() {
+    return (
+      <View style={styles.frame}>
+        <ImageBackground source={require('./flowers.png')} style={styles.backgroundImage}>
+          <View style={styles.container}>
+            <Text style={styles.welcome}>
+              You input {this.state.zip}.
+            </Text>
+            <TextInput
+              style={styles.input}
+              onSubmitEditing={this._handleTextChange}
+            />
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  frame: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  input: {
+    fontSize: 20,
+    borderWidth: 2,
+    padding: 2,
+    height: 40,
+    width: 100,
+    textAlign: 'center',
+  },
+  backgroundImage: {
+      flex: 1,
+  },
+});
