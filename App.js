e578b884265014b0d8df3c284ebbaff4a6d5a55d
Zatic Prasertarcha <coach.psac@gmail.com>
2018-03-01 14:15:17 +0700

    done

--- App.js
+++ App.js
@@ -11,8 +11,7 @@ import {
   Text,
   View,
   FlatList,
-  ScrollView,
-  ListView
+  Image
 } from 'react-native';
 
 import ScrollApp from './ScrollApp'
@@ -28,22 +27,41 @@ type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
     super(props);
-    this.state = {
-      data:ContactsData.fetchContacts().then()
-    }
+    this.state = { data:[]}
+    ContactsData.fetchContacts()
+      .then(contacts=>{console.log(contacts);
+      this.setState({data:contacts});})
+      .catch(error=>{console.log(error)});
+  }
+
+  _renderContact = ({item})=>{
+    const picsrc = item.firstname.toLowerCase()+'.jpeg'
+    const imgurl = 'http://web.sit.kmutt.ac.th/sanit/int493/contacts/img/'+picsrc
+    return (
+      <View style={styles.row}>
+        <Image style={styles.picture} source={{url:imgurl}}/>
+          <View style={{flexDirection:'column'}}>
+            <View style={{flexDirection:'row'}}>
+              <Text style={styles.label}>{item.prefix}</Text>
+              <Text style={styles.label}>{item.firstname} </Text>
+              <Text style={styles.label}>{item.lastname}</Text>
+            </View>
+            <Text style={styles.grey}>Tel : {item.phone}</Text>
+          <Text style={styles.grey}>Email : {item.email}</Text>
+          </View>
+      </View>
+    )
   }
   
   render() {
     return (
-      // <ScrollApp/>
-      // <FruitApp/>
-      // <FruitApp2/>
-      // <FlatApp/>
       <View style={styles.container}>
-        <Text>
-          {this.state.data}
-          </Text>
-        </View>
+      <FlatList
+      data = {this.state.data}
+      renderItem={this._renderContact}
+      keyExtractor={(item,index) => index.toString()}
+      />
+      </View>
     )
   }
 }
@@ -51,19 +69,27 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop:20,
+    justifyContent:'center',
+    alignItems:'stretch',
+    backgroundColor:'#F5FCFF'
   },
-  boxSmall: {
-    width: 200,
-    height: 200,
-    marginBottom: 10,
-    marginRight: 10,
-    backgroundColor: 'skyblue',
+  label:{
+    fontSize:16
   },
-  boxLarge: {
-    width: 300,
-    height: 300,
-    marginBottom: 10,
-    marginRight: 10,
-    backgroundColor: 'steelblue',
+  grey:{
+    color:'grey'
   },
+  picture:{
+      height:50,
+      width:50,
+      resizeMode:'contain'
+  },
+  row:{
+    flexDirection:'row',
+    alignItems:'center',
+    padding:5,
+    borderWidth:1,
+    borderColor:'#DDDDDD',
+  }
 }) ;
--- ContactsData.js
+++ ContactsData.js
@@ -1,14 +1,14 @@
 function fetchContacts() {
-    return fetch('http://web.sit.kmutt.ac.th/sanit/int493/contacts.php')
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson.contacts;
-    })
+    const url="http://web.sit.kmutt.ac.th/sanit/int493/contacts.php"
+    return fetch(url)
+        .then(response => response.json())
+        .then((responseJson) => {
+            return responseJson.contacts;
+        })
         .catch(error => {
             console.error(error);
         });
 }
-
 export default {
     fetchContacts: fetchContacts
 };
